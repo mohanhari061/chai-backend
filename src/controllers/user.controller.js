95a6b5d8952c3094dd7426b768debb685d331d83
Hari Mohan <mohanhari061@gmail.com>
2025-03-04 03:14:05 +0530

    acces and refresh token endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,10 @@
 import  {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js";
 import User from "../models/user.model.js";
-import cloudinary from "cloudinary";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/ApiResponse.js";  
+import jwt from 'jsonwebtoken'
+
 
 const generateAccessAndRefreshTokens=async (userId)=>{
     try {
@@ -107,7 +108,7 @@ const loginUser=asyncHandler(async(req,res)=>{
 
     const {email,username,password}=req.body
 
-    if(!username || !email){
+    if(!(username || email)){
         throw new ApiError(400, "username or email is required")
     }
 
@@ -179,5 +180,51 @@ const logoutUser=asyncHandler(async (req,res)=>{
 })
 
 
+const refreshAccessToken=asyncHandler(async (req,res)=>{
+    const incomingRefreshToken=req.cookies.refreshAccessToken || req.body.refreshToken
+    if(!incomingRefreshToken){
+        throw new ApiError(401,"Unauthorised request")
+    }
 
-export {registerUser, loginUser, logoutUser}
+    try {
+        const decodedToken=jwt.verify(incomingRefreshToken,process.env.REFRESH_TOKEN_SECRET)
+        
+        const user=await User.findById(decodedToken?._id)
+        if(!user){
+            throw new ApiError(401,"Invlaid refresh token")
+        }
+    
+        if(incomingRefreshToken!==user?.refreshToken){
+            throw new ApiError(401,"Refresh token is expired or used")
+        }
+    
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+    
+        const {newAccessToken,newRefreshToken}=await generateAccessAndRefreshTokens(user._id)
+    
+        return res
+        .status(200)
+        .cookie("accessToken",newAccessToken,options)
+        .cookie("refreshToken",newRefreshToken,options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken:newAccessToken,
+                    refreshToken:newRefreshToken
+                },
+                "Access token refreshed"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(401,error?.message || "Invalid refresh token")
+    }
+    
+
+})
+
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,11 +56,11 @@ userSchema.pre("save",async function (next){
 });
 
 
-userSchema.methods.isPasswordCorrect=async (password)=>{
+userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password, this.password);
 }
 
-userSchema.methods.generateAccessToken = ()=>{
+userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
         {
             _id: this._id,
@@ -72,7 +72,7 @@ userSchema.methods.generateAccessToken = ()=>{
         {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
     );
 }
-userSchema.methods.generateRefreshToken = ()=>{
+userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
             _id: this._id,
